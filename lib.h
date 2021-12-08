38578df6917ec3d17070f85429f3e89f92360072
Lindmend <91802623+Lindmend@users.noreply.github.com>
2021-12-08 22:28:32 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by lindmend on 08/12/2021.
+//
+
+#ifndef UNTITLED15_LIB_H
+#define UNTITLED15_LIB_H
+float numeroMassimo(const float numeri[], int dimensione);
+#endif //UNTITLED15_LIB_H
 
